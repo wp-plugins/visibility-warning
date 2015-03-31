1ecc22173e289d0241938832770f08324ec4a163
pauiglesias <pauiglesias@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-31 22:27:04 +0000

    Updating trunk directory to 1.1 version.
    git-svn-id: https://plugins.svn.wordpress.org/visibility-warning/trunk@1124892 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- visibility-warning.php
+++ visibility-warning.php
@@ -1,68 +1,50 @@
 <?php
 /*
-Plugin Name: Visibility Warning
+Plugin Name: Search Engine Visibility Warning
 Plugin URI: http://blogestudio.com/
 Description: Show a warning message in admin header area when your blog visibility is blocked for search engines. Also, when your blog is visible for everyone, hides the menu link to privacy options to prevent accidental changes.
 Author: Pau Iglesias, Blogestudio
-Author URI: http://blogestudio.com/
-Version: 1.0.1
+Version: 1.1
 Text Domain: visibility-warning
-License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+License: GPLv2 or later
 */
-
-/*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
-
 
 // Avoid direct script calls via plugin URL
-if ( ! function_exists( 'add_action' ) )
+if (!function_exists('add_action'))
 	die;
 
 // This version only works in admin area
-if ( ! is_admin() )
+if (!is_admin())
 	return;
 
-
-
 /**
- * Visibility warning plugin class
+ * Visibility Warning plugin class
  *
  * @package WordPress
  * @subpackage Visibility Warning
  */
 
 // Avoid declaration plugin class conflicts
-if ( ! class_exists( 'CBlogestudioVisibilityWarning' ) ) {
+if (!class_exists('BE_Visibility_Warning')) {
+
+	// Create object plugin
+	add_action('init', array('BE_Visibility_Warning', 'instance'));
 
 	// Main and unique class
-	class CBlogestudioVisibilityWarning {
+	class BE_Visibility_Warning {
 
 
 
-		// Save blog_public option state
-		var $blog_public = false;
+		// Initialization
+		// ---------------------------------------------------------------------------------------------------
 
 
 
 		/**
-		 * PHP 4 initialization compatibility
+		 * Creates a new object instance
 		 */
-		function CBlogestudioVisibilityWarning() {
-			$this->__construct();
+		public static function instance() {
+			return new BE_Visibility_Warning;
 		}
 
 
@@ -70,20 +52,26 @@ if ( ! class_exists( 'CBlogestudioVisibilityWarning' ) ) {
 		/**
 		 * Initialize plugin
 		 */
-		function __construct() {
+		private function __construct() {
 			
 			// Check blog public option
-			$this->blog_public = get_option( 'blog_public' );
-			if ( false !== $this->blog_public ) {
+			$blog_public = get_option('blog_public');
+			if (false !== $blog_public) {
 				
-				// Blog visible for search engines
-				if ( $this->blog_public == 1 )
-					add_action( 'admin_init', array( $this, 'remove_submenu_page' ), 100 );
+				// Visible for search engines
+				if ($blog_public != 0) {
+					
+					// For old WP versions, hide submenu Options Privacy
+					add_action('admin_init', array($this, 'remove_submenu_page'), 100);
+					
+					// For recent WP versions, hide Visibility options in Settings > reading
+					add_action('current_screen', array($this, 'check_current_screen'));
 				
 				// Blocked for search engines
-				elseif ( $this->blog_public == 0 ) {
-					add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
-					add_action( 'admin_notices', array( $this, 'show_warning' ) );
+				} elseif ($blog_public == 0) {
+					
+					// Show warning messages in header area
+					add_action('admin_notices', array($this, 'show_warning'));
 				}
 			}
 		}
@@ -93,30 +81,63 @@ if ( ! class_exists( 'CBlogestudioVisibilityWarning' ) ) {
 		/**
 		 *  Load translation file
 		 */
-		function load_plugin_textdomain() {
+		private function load_plugin_textdomain() {
+			
+			// Check load
+			static $loaded;
+			if (isset($loaded))
+				return;
+
+			// Mark as loaded
+			$loaded = true;
 			
 			// Check if this plugin is placed in wp-content/mu-plugins directory
-			if ( basename( dirname ( dirname( __FILE__ ) ) ) == 'mu-plugins' ) {
+			if ('mu-plugins' == basename(dirname(dirname(__FILE__))) || 'mu-plugins' == basename(dirname(__FILE__))) {
 				
 				// Check WP version support
-				if ( function_exists( 'load_muplugin_textdomain' ) )
-					load_muplugin_textdomain( 'visibility-warning', basename( dirname( __FILE__ ) ) . '/languages');
+				if (function_exists('load_muplugin_textdomain')) {
+					load_muplugin_textdomain('visibility-warning', basename(dirname(__FILE__)).'/languages');
+					return;
+				}
 			}
 			
 			// Usual wp-content/plugins directory location
-			else load_plugin_textdomain( 'visibility-warning', false, basename( dirname( __FILE__ ) ) . '/languages');
+			load_plugin_textdomain('visibility-warning', false, basename(dirname(__FILE__)).'/languages');
 		}
+
+
+
+		// Hooks
+		// ---------------------------------------------------------------------------------------------------
 
 
 
 		/**
 		 * Remove Privacy link from Settings menu section
+		 * This is a feature compatible with old WP versions
 		 */
-		function remove_submenu_page() {
+		public function remove_submenu_page() {
+			if (function_exists('remove_submenu_page'))
+				remove_submenu_page('options-general.php', 'options-privacy.php');
+		}
 
-			// Check WP version support
-			if ( function_exists( 'remove_submenu_page' ) )
-				remove_submenu_page( 'options-general.php', 'options-privacy.php' );
+
+
+		/**
+		 * Check options-reading.php admin page
+		 */
+		public function check_current_screen($current_screen) {
+			if ('options-reading' == $current_screen->id)
+				add_action('admin_print_styles', array($this, 'hide_visibility_options'));
+		}
+
+
+
+		/**
+		 * Hide options from options-reading.php admin page
+		 */
+		public function hide_visibility_options() {
+			echo '<style type="text/css">.option-site-visibility{display:none}</style>';
 		}
 
 
@@ -124,19 +145,19 @@ if ( ! class_exists( 'CBlogestudioVisibilityWarning' ) ) {
 		/**
 		 * Show warning in admin header area
 		 */
-		function show_warning() {
-			echo '<div id="visibility-warning" class="updated fade"><p><strong>' . __( 'Your site is blocked for search engines.', 'visibility-warning' ) . '</strong> ' . sprintf( __( 'If you want to be visible for everyone go to <a href="%s">Privacy settings</a> or notify to site admins.', 'visibility-warning' ), 'options-privacy.php' ) . '</p></div>' . "\n";
+		public function show_warning() {
+			
+			// Load translations
+			$this->load_plugin_textdomain();
+			
+			// Detect old versions
+			$old_versions = version_compare(get_bloginfo('version'), '3.5', '<');
+			
+			// Link and anchor depends of WP version
+			echo '<div id="visibility-warning" class="updated fade"><p><strong>'.__('Your site is blocked for search engines.','visibility-warning').'</strong> '.sprintf(__('If you want to be visible for everyone go to <a href="%s">%s</a> or notify to site admins.', 'visibility-warning'), admin_url($old_versions? 'options-privacy.php' : 'options-reading.php'), $old_versions? __('Privacy Settings', 'visibility-warning') : __('Reading Settings', 'visibility-warning')).'</p></div>'."\n";
 		}
 
 
 
 	}
-
-	// Start and set in globals
-	$GLOBALS['visibility_warning'] = new CBlogestudioVisibilityWarning();
-
 }
-
-
-
-?>
